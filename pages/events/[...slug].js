586dda732b7fab42309791823dbe0ddb83e5f454
wonsy1006 <wonsy1006@gmail.com>
2023-01-26 23:54:48 +0900

    navigating filtered events page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,12 @@
+import { useRouter } from "next/router";
+
 function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  console.log(filterData);
+
   return (
     <div>
       <h1>Filtered Events</h1>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,25 @@
-import Link from "next/link";
+import { Fragment } from "react";
 import { useRouter } from "next/router";
+
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
 function AllEventsPage() {
   const router = useRouter();
-  console.log(router.query);
+  const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}/`;
+
+    router.push(fullPath);
+  }
 
   return (
-    <div>
-      <h1>All Events</h1>
-    </div>
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
   );
 }
 
